7c36b9e8cc29a5bc976bc04ad19bc2c25dfc5679
Fergus Fitzpatrick <fergusfitz@gmail.com>
2016-11-18 16:41:12 +0000

    Not much added
    Simple update to head in direction that user can perform a particular
    operation.

--- Maths.java
+++ Maths.java
@@ -8,8 +8,15 @@ import java.util.*;
 public class Maths{
 	public static void main (String args[]){
 		Scanner scan = new Scanner (System.in);
+
+		//read in variable and command.
+		System.out.println("Please enter your first integer: ");
 		int x = scan.nextInt();
+		System.out.println("Please enter your second integer: ");
 		int y = scan.nextInt();
+
+		// take in the operator in symbol form. - need to re-write to improve ! and prime.
+		System.out.println("Please enter your either +, - , *, /, !, % or prime  : ");
 
 		MathsFunc.add(x, y);
 		MathsFunc.divide(x, y);
